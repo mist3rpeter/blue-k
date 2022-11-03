fda5d7147c47af2299319c67cf798c7026bed6de
Alexandre Coreixas <coreixas@iMac-de-Hugo.local>
2022-11-03 14:00:08 -0300

    aula48

--- sketch.js
+++ sketch.js
@@ -9,13 +9,24 @@ var bb8
 var fogareo
 var nuvemm
 var go='jogar'
+var precioso
+var bitcoin
+var ss
+var points
+var pontos=0
+var replay
+
 function preload(){ // função que carregar todas as imagens e animações
   sky= loadImage('assets/bg.png')
   gi= loadImage('assets/ground.png')
   ci= loadImage('assets/nuvem.png')
   rb= loadAnimation('assets/flint1.png ','assets/flint2.png','assets/flint3.png')
   fuego= loadAnimation('assets/fire1.png ','assets/fire2.png')
-  //bb8= loadImage('assets/bot.png')
+  bb8= loadImage('assets/obstaculo.png')
+  precioso= loadImage('assets/moeda.png')
+  bitcoin= loadImage('assets/moeda2-1.png')
+  points= loadImage('assets/text.png')
+  replay= loadImage('assets/restart.png')
 }
 
 function setup(){ // todas as configuraçoes dos objetos
@@ -29,14 +40,15 @@ function setup(){ // todas as configuraçoes dos objetos
   flint.setCollider('rectangle',0,0,5,35)
   ground.setCollider('rectangle',0,40,600,20,)
   fogareo= new Group()
-  
+  ss= new Group()
 }
 function draw(){
   background(sky);
   drawSprites(); 
-
-  claudia()
-  fogao()
+ image(points, 460, 10, 100, 100)
+  textSize(25)
+  text(pontos, 570, 60)
+  
   if(keyDown('space')&&flint.y>220     ){
  flint.velocityY=-9
   }
@@ -49,6 +61,20 @@ function draw(){
   fogareo.setVelocityXEach(0)
   
   }
+  if(go==='jogar'){
+    claudia()
+    fogao()
+    ouro()
+    flint.overlap(ss,function(colletor, coletado){
+    pontos= pontos +1
+    coletado.remove()
+
+
+    })
+  }
+ 
+
+
 }
 function claudia(){
 if(frameCount%100===0){
@@ -63,11 +89,41 @@ cladio.depth= flint.depth -1
 
 function fogao(){
 if(frameCount%150===0){
-var rengoku= createSprite(700,400,10,10)
-//rengoku.addAnimation('CHAMAS',fuego)
+var rengoku= createSprite(700,415,10,10)
+rengoku.addImage('CHAMAS',bb8)
 rengoku.scale=5
 rengoku.velocityX=-7
 rengoku.debug=true
 fogareo.add(rengoku)
+rengoku.setCollider('rectangle',0,0,10,20)
 }
 }
+
+function ouro(){
+  var aux=Math.round(random(50,200))
+
+  if(frameCount%aux===0){
+  var ferrari= createSprite(775,random(300,415),10,10)
+  ferrari.scale=5
+  ferrari.velocityX=-7
+  ferrari.debug=true
+  ss.add(ferrari)
+  ferrari.setCollider('rectangle',0,0,10,20)
+  var key= Math.round(random(1,2))
+  switch (key) {
+    case 1:ferrari.addImage('money',precioso)
+      
+      break;
+    
+    case 2: ferrari.addImage('coin',bitcoin)
+    default:
+      break;
+  }
+  }
+  }
+  
+
+
+
+
+
